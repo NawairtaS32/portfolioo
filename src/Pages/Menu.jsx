b0306e43bb522f67ed7eab4c7cfebdec547f63e7
I Gusti Made Bagas Satriawan <igustimade98@gmail.com>
2023-07-25 23:31:33 +0700

    memperbaikin menu

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -4,99 +4,86 @@ import { FcContacts } from "react-icons/fc";
 import { AiOutlineFundProjectionScreen, AiOutlineClose } from "react-icons/ai";
 import { GrVulnerability } from "react-icons/gr";
 import { FiMenu } from "react-icons/fi";
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 
+const menuItems = [
+    {
+        id: 1,
+        icon: <FaUserCircle className="text-[50px] text-white" />,
+        to: "about",
+        label: "About",
+    },
+    {
+        id: 2,
+        icon: <GrVulnerability className="text-[50px] text-slate-900 bg-green-900 p-1 rounded-full" />,
+        to: "skills",
+        label: "Skills",
+    },
+    {
+        id: 3,
+        icon: <AiOutlineFundProjectionScreen className="text-[50px] text-white bg-slate-600 hover:bg-blue-600 p-1 rounded-2xl" />,
+        to: "projects",
+        label: "Projects",
+    },
+    {
+        id: 4,
+        icon: <FcContacts className="text-[50px]" />,
+        to: "contact",
+        label: "Contact",
+    },
+];
 
-
-export default function Menu(){
-
-    const [show, setShow] = useState(false)
-    const [sticky, setSticky] = useState(false);
+export default function Menu() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isSticky, setIsSticky] = useState(false);
 
     useEffect(() => {
-        window.addEventListener("scroll", () => {
-          const button = document.querySelector("button");
-          window.scrollY > 0 ? setSticky(true) : setSticky(false);
-        });
+        const handleScroll = () => {
+        window.scrollY > 0 ? setIsSticky(true) : setIsSticky(false);
+        };
+        window.addEventListener("scroll", handleScroll);
+
+        // Cleanup the event listener when the component unmounts
+        return () => {
+        window.removeEventListener("scroll", handleScroll);
+        };
     }, []);
 
-    const handleShowModal = ()=>{
-        setShow(!show)
-    }
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
-
-    
     return (
         <>
-            {show && (
-                    <div className="max-w-lg mx-auto" >
-                        <div className={`fixed bottom-[24%] bg-blue-400/60 border-double border-4 px-10 rounded-l-[60px] grid grid-cols-1 gap-8 p-8 -right-[0px] text-white z-[200]
-                                `}  >
-                            <Link
-                                to= "about"
-                                smooth={true}
-                                offset={-100}
-                                duration={500}
-                                onClick={handleShowModal}
-                            >
-                                <div className="flex flex-col justify-center items-center">
-                                    <FaUserCircle className="text-[50px] text-white" />
-                                </div>
-                            </Link>
-                            
-                            <Link
-                                to= "skills"
-                                smooth={true}
-                                offset={-40}
-                                duration={500}
-                                onClick={handleShowModal}
-                            >
-                                <div className="flex flex-col justify-center items-center">
-                                    <GrVulnerability className="text-[50px] text-slate-900 bg-green-900 p-1 rounded-full" />
-                                </div>
-                            </Link>
+        <div
+            className={` max-w-lg mx-auto fixed bottom-[24%] right-0 bg-blue-400/60 border-double border-4 px-10 rounded-l-[60px] grid grid-cols-1 gap-4 p-8 text-white z-50 ${
+            isMenuOpen ? "menu-open" : "menu-closed"
+            }`}
+        >
+            <div className="flex flex-col gap-6 items-center w-full">
+            {menuItems.map((item) => (
+                <Link key={item.id} to={item.to} smooth={true} offset={-40} duration={500}>
+                <div className="flex flex-col justify-center items-center">
+                    {item.icon}
+                    <span>{item.label}</span>
+                </div>
+                </Link>
+            ))}
+            </div>
+        </div>
 
-                            <Link
-                                to= "projects"
-                                smooth={true}
-                                offset={-40}
-                                duration={1500}
-                                onClick={handleShowModal}
-                            >
-                                <div className="flex flex-col justify-center items-center">
-                                    <AiOutlineFundProjectionScreen className="text-[50px] text-white bg-slate-600 hover:bg-blue-600 p-1 rounded-2xl" />
-                                </div>
-                            </Link>
-
-                            <Link
-                                to= "contact"
-                                smooth={true}
-                                offset={-40}
-                                duration={500}
-                                onClick={handleShowModal}
-                            >
-                                <div className="flex flex-col justify-center items-center">
-                                    <FcContacts className="text-[50px]" />
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
+        <button
+            className={`w-[60px] h-[60px] font-bold rounded-3xl fixed top-[1%] right-[4%] flex justify-center items-center z-50 lg:hidden ${
+            isSticky ? "text-black" : "lg:bg-transparent text-white"
+            }`}
+            onClick={toggleMenu}
+        >
+            {isMenuOpen ? (
+            <AiOutlineClose className="text-[28px]" />
+            ) : (
+            <FiMenu className="text-[28px]" />
             )}
-        
-            <button className={`w-[60px] h-[60px] font-bold  rounded-3xl fixed top-[1%] 
-                                right-[4%] flex justify-center items-center z-[250] flex lg:hidden
-                                ${
-                                    sticky ? "text-black" : "lg:bg-transparent text-white"
-                                } 
-                                `} 
-                    onClick={handleShowModal}>
-                {
-                    show ?
-                    <AiOutlineClose className="text-[28px] " />
-                    :
-                    <FiMenu className="text-[28px] " />
-                }
-            </button>
+        </button>
         </>
-    )
+    );
 }
